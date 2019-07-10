a83c81878c476cb3113dfdc14028d945dc8667cc
Kyle Koch <kylekoch18@gmail.com>
2019-07-10 00:30:04 -0500

    Add JS, Insert Comments for greeting function

---
+++ script.js
@@ -0,0 +1,18 @@
+// ************ Homework *************
+// Creat a greeter function for you project that asks for a users name//
+// stores that name in a variable, and then displays the //
+// variable as the <H1> on the homepage //
+
+
+/* 
+1- create a variable
+2- assigned to prompt("What is your name? ")
+3- use document.querySelectors to replace the H1 on your page to the variable above  
+4- Have your heading be a greeting, "Hello, ______ , Welcom to the page" Concantenate the variables and strings
+*/
+
+// let name = prompt("What is your name?");
+
+// OLD WAY ES5 --- let fullname = fname + " " + lname;
+// NEW WAY ES6 ---let fullname = `Hello, ${fname} ${lname}! Welcome!`
+
