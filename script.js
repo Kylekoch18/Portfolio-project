77214c88fc105f8172172748950a7f1bfa5eb096
Kyle Koch <kylekoch18@gmail.com>
2019-07-13 12:47:53 -0500

    add greetings promt to portfolio project

--- script.js
+++ script.js
@@ -16,3 +16,25 @@
 // OLD WAY ES5 --- let fullname = fname + " " + lname;
 // NEW WAY ES6 ---let fullname = `Hello, ${fname} ${lname}! Welcome!`
 
+
+//const h1 = document.querySelector(`h1`)
+//h1.textContent = `Hello, ${name}
+
+const name = prompt(`What is your name?`);
+
+
+const x = 3;
+const dessert = 'icecream';
+
+if(x === 3){
+    console.log('3 is the lucky number');
+}
+else if(x === 7){
+    console.log('wrong numba');
+}
+else {
+    console.log('no sir');
+}
+
+document.querySelector(`h1`).textContent = `Hello, ${name}`;
+
