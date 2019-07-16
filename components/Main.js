c21b8a5ccc335fe345eb8af4f720e6c5e0390c66
Kyle Koch <kylekoch18@gmail.com>
2019-07-15 20:37:34 -0500

    Create all modules with 'export default'

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+export default `
+<main>
+  <section>
+  <h2>this is my top section.</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum perspiciatis corporis exercitationem sint dolore quasi quas? Quam, architecto sed modi ea incidunt dolor, voluptatem animi repudiandae sint ut repellendus?</p>
+<a href="#" class="cta-btn">Read More!</a>
+</section>
+<section>
+    <h2>this is my top section.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum perspiciatis corporis exercitationem sint dolore quasi quas? Quam, architecto sed modi ea incidunt dolor, voluptatem animi repudiandae sint ut repellendus?</p>
+    <a href="#" class="cta-btn">Read More!</a>
+  </section>
+  <section>
+      <h2>this is my top section.</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum perspiciatis corporis exercitationem sint dolore quasi quas? Quam, architecto sed modi ea incidunt dolor, voluptatem animi repudiandae sint ut repellendus?</p>
+      <a href="#" class="cta-btn">Read More!</a>
+    </section>
+</main>
+`;
