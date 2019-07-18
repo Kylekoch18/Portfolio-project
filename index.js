4244eaf29c003db029573e97c9f4e6f5a9830acf
Kyle Koch <kylekoch18@gmail.com>
2019-07-17 20:57:11 -0500

    Re-render the page with the correct title based on a click event

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-import Navigation from './components/Nav';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/footer';
+import Navigation from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/footer";
 
-const state = {
-    'title': 'This is the page Title!'
+const states = {
+  'home': {
+    'title': "This is the page Title!"
+  },
+  'about': {
+    'title': "About Page"
+  },
+  'contact': {
+    'title': "Contact"
+  }
+};
+
+render(states.home);
+
+const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
+
+function render(state){
+    document.querySelector("#root").innerHTML = `
+    ${Navigation(state)}
+    ${Header(state)}
+    ${Main(state)}
+    ${Footer(state)}
+    `;
 }
 
 
-document.querySelector('#root').innerHTML = `
-${Navigation()}
-${Header(state)}
-${Main()}
-${Footer()}
-`;
 
-console.log(Navigation, Header, Main, Footer);
+let i = 0;
+
+while(i < navItems.length) {
+    navItems[i].addEventListener('click', function clickHandler(event){
+event.preventDefault();
+
+const clickedItems = event .target.textContent;
+const clicked = clickedItems.toLowerCase();
+    
+render(states[clicked]);
+});
+i++;
+}
