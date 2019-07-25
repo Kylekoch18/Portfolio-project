a11d4352fa7e7043fba4835a1f8eb05a47011d84
Kyle Koch <kylekoch18@gmail.com>
2019-07-24 19:55:09 -0500

    commit things out in index.js

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ import Main from "./components/Main";
 import Footer from "./components/footer";
 
 const states = {
-  'home': {
-    'title': "This is the page Title!"
+  home: {
+    title: "This is the page Title!"
   },
-  'about': {
-    'title': "About Page"
+  about: {
+    title: "About Page"
   },
-  'contact': {
-    'title': "Contact"
+  contact: {
+    title: "Contact"
   }
 };
 
-render(states.home);
-
-const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
-
-function render(state){
-    document.querySelector("#root").innerHTML = `
+function render(state) {
+  document.querySelector("#root").innerHTML = `
     ${Navigation(state)}
     ${Header(state)}
     ${Main(state)}
@@ -28,18 +24,38 @@ function render(state){
     `;
 }
 
+// to render a page. we pass in a piece of state
+render(states.home);
 
+//the elementss will not exist until page is rendered
+const navItems = document.querySelectorAll("nav > ul > li:not(.dropdown)");
 
-let i = 0;
-
-while(i < navItems.length) {
-    navItems[i].addEventListener('click', function clickHandler(event){
-event.preventDefault();
-
-const clickedItems = event .target.textContent;
-const clicked = clickedItems.toLowerCase();
-    
-render(states[clicked]);
+navItems.forEach(function(navItem) {
+  navItem.addEventListener('click', function() {
+    event.preventDefault();
+    render(states[event.target.textContent.toLowerCase()]);
+  });
 });
-i++;
+
+
+/*
+//i will run from 0 until 3
+while (i < navItems.length) {
+  //use the value of i as an index to access the navitem
+  navItems[i].addEventListener(
+    "click",
+
+    //add this callback fxn to each of the navItems
+    function clickHandler(event) {
+      event.preventDefault();
+
+      const clickedItems = event.target.textContent;
+      const clicked = clickedItems.toLowerCase();
+
+      render(states[event.target.textContent.toLowerCase()]);
+      // Developers Note: render(states[event.target.textContent.toLowerCase()]);
+    }
+  );
+  i++;
 }
+*/
