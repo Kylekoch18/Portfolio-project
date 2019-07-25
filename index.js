8a36f98dfd60bb6cf35341b4aaa593e198898fc6
Kyle Koch <kylekoch18@gmail.com>
2019-07-24 20:01:17 -0500

    deleted savvy-bookstore-hack-a-thon

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import Navigation from "./components/Nav";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/footer";
+import Navigation from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/footer';
 
+<<<<<<< HEAD
 const states = {
   home: {
     title: "This is the page Title!"
@@ -22,6 +23,10 @@ function render(state) {
     ${Main(state)}
     ${Footer(state)}
     `;
+=======
+const state = {
+    'title': 'This is the page Title!'
+>>>>>>> parent of 4244eaf... Re-render the page with the correct title based on a click event
 }
 
 // to render a page. we pass in a piece of state
@@ -30,6 +35,7 @@ render(states.home);
 //the elementss will not exist until page is rendered
 const navItems = document.querySelectorAll("nav > ul > li:not(.dropdown)");
 
+<<<<<<< HEAD
 navItems.forEach(function(navItem) {
   navItem.addEventListener('click', function() {
     event.preventDefault();
@@ -59,3 +65,13 @@ while (i < navItems.length) {
   i++;
 }
 */
+=======
+document.querySelector('#root').innerHTML = `
+${Navigation()}
+${Header(state)}
+${Main()}
+${Footer()}
+`;
+
+console.log(Navigation, Header, Main, Footer);
+>>>>>>> parent of 4244eaf... Re-render the page with the correct title based on a click event
