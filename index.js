c9c3a7f73ae24a6c66753a39dd94687ea1981db0
Kyle Koch <kylekoch18@gmail.com>
2019-07-29 19:01:49 -0500

    Add npm run script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/footer';
 
-<<<<<<< HEAD
+
 const states = {
   home: {
     title: "This is the page Title!"
@@ -23,10 +23,6 @@ function render(state) {
     ${Main(state)}
     ${Footer(state)}
     `;
-=======
-const state = {
-    'title': 'This is the page Title!'
->>>>>>> parent of 4244eaf... Re-render the page with the correct title based on a click event
 }
 
 // to render a page. we pass in a piece of state
@@ -35,7 +31,6 @@ render(states.home);
 //the elementss will not exist until page is rendered
 const navItems = document.querySelectorAll("nav > ul > li:not(.dropdown)");
 
-<<<<<<< HEAD
 navItems.forEach(function(navItem) {
   navItem.addEventListener('click', function() {
     event.preventDefault();
@@ -65,13 +60,3 @@ while (i < navItems.length) {
   i++;
 }
 */
-=======
-document.querySelector('#root').innerHTML = `
-${Navigation()}
-${Header(state)}
-${Main()}
-${Footer()}
-`;
-
-console.log(Navigation, Header, Main, Footer);
->>>>>>> parent of 4244eaf... Re-render the page with the correct title based on a click event
